83d21d7cc89df057638f6148ed2d114083d549c1
IgleDev <adriiglesias2016@gmail.com>
2024-02-25 00:41:19 +0100

    Todas las funcionalidades hechas!

--- js/auth.js
+++ js/auth.js
@@ -3,9 +3,11 @@ const button = document.getElementById('generateQR');
 // Función para validar que el campo del input la URL no está vacia ni tiene un mal formato.
 button.onclick = () => {
     const input = document.getElementById('inputtxt').value;
-    if(input === '' || input === null || !input.match(/^https?:\/\/([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/i)) {
+
+    if(input === ' ' || input === null || !input.match(/^https?:\/\/([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/i)) {
         alert('Tienes que introducir una URL válida');
     } else {
+        localStorage.setItem('input', input);
         window.location.href = './qr.html'; // Lo mandamos a nuestra página donde aplicaremos la librería.
     }
 }
--- js/qrcode.js
+++ js/qrcode.js
@@ -1,25 +1,37 @@
-var qrcode = new QRCode("qrcode");
+const div = document.getElementById('qrcode');
+const input = localStorage.getItem('input');    // Cojemos el input guardado en el LocalStorage
+const btnDownload = document.getElementById('btnDown');
+const btnCopyQR = document.getElementById('btnCopyQR');
 
-function makeCode () {    
-  var elText = document.getElementById("text");
-  
-  if (!elText.value) {
-    alert("Input a text");
-    elText.focus();
-    return;
-  }
-  
-  qrcode.makeCode(elText.value);
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	text: input,
+	width: 250,
+	height: 250,
+	colorDark : "#000000",
+	colorLight : "#ffffff",
+    id : 'qrImg',
+	correctLevel : QRCode.CorrectLevel.H
+});
+
+//Creamos un función para que cuando hagamos click se nos guarde
+btnDownload.onclick = () => {
+    let img = document.querySelectorAll('img')[1].src;  //Buscamos la imagen en el array y cogemos su src
+    let linkImg = document.createElement('a');  //Creamos un a para hacer el efecto download
+    linkImg.href = img; //Le pasamos la imagen como referencia
+    linkImg.download = `qrcode${input}`;    //Cuando la descarguemos esta tendra 'qrcode' más el texto
+    linkImg.target = '_blank';  // Ponemos el atributo blank para que no tengamos que ir a otra página
+    linkImg.click();    // Simulamos el boton click de un elemento a y ya se nos descarga
 }
 
-makeCode('ola');
-
-$("#text").
-  on("blur", function () {
-    makeCode();
-  }).
-  on("keydown", function (e) {
-    if (e.keyCode == 13) {
-      makeCode();
+btnCopyQR.onclick = () => {
+    let pag = window.location;
+    const copiarContenido = async () => {
+        try {
+          await navigator.clipboard.writeText(pag);
+          console.log('Contenido copiado al portapapeles');
+        } catch (err) {
+          console.error('Error al copiar: ', err);
+        }
     }
-  });
+    copiarContenido();
+}
